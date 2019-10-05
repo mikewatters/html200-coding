5fd1a30a9b4725c10e307989dda37e2023731dc5
Admin <admin@Admins-MacBook-Pro.local>
2019-10-05 07:19:55 -0700

    Added assignment 1

---
+++ Lesson02/calculator.js
@@ -0,0 +1,16 @@
+let x = 'panther';
+const y = 0;
+const z = 4;
+const a = 100;
+const b = 1001;
+let c = 'the cat';
+let d = '4';
+
+console.log(b % a);
+console.log(x + ' ' + z);
+console.log((a + b) * z);
+console.log(a + b * z);
+console.log(z * a);
+console.log(x + ' ' + c);
+console.log(z + d);
+console.log(z + z);
