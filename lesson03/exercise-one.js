3ab74a06f90aea0f96fcc70ccd7e06e08f72935f
Admin <admin@Admins-MacBook-Pro.local>
2019-10-06 09:11:30 -0700

    Exercise 1

---
+++ lesson03/exercise-one.js
@@ -0,0 +1,9 @@
+let pets = ['corgi', 'collie', 'domestic longhair cat', 'pomeranian', 'tuxedo cat']
+
+pets.push('orange tabby cat');
+
+pets.splice(2, 1);
+
+const petsList = pets.join(',')
+
+console.log(pets);
